8c225abc26ba573c1df233afb8bb4267d5aa382e
Benjamin <benjamin.vandeginste@gramontinnovation.com>
2022-01-02 19:23:51 +0100

    fastloading...

--- src/tamperStuff.js
+++ src/tamperStuff.js
@@ -4,17 +4,14 @@ exports.mod = () => {
     let db_globals = fileIO.readParsed(db.base.globals);
 
     if (config.fastloading.enable){
-
-        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier");
+        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier.");
         // set loading to default / multiplier
         db_globals.config.BaseLoadTime = parseFloat((0.85/config.fastloading.speed_multiplier).toFixed(2));
-        logger.logSuccess(db_globals.config.BaseLoadTime);
-
     }else{
-
-        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed");
+        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed.");
         // set loading to default
         db_globals.config.BaseLoadTime = 0.85;
     }
+    logger.logSuccess("[MOD] Magazine Maniac loaded.")
     fileIO.write(db.base.globals, db_globals);
 }
