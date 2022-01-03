8bd6e6e0634147d4fb453fe8b84f54468bf0a8bd
Nehaxfr <benji.vandeginste@gmail.com>
2022-01-03 20:11:27 +0100

    details

---
+++ src/MagazineManiac.js
@@ -0,0 +1,17 @@
+exports.mod = () => {
+    logger.logSuccess("[MOD] Magazine Maniac loading...");
+    let config = require("../config.json");
+    let db_globals = fileIO.readParsed(db.base.globals);
+
+    if (config.fastloading.enable){
+        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier.");
+        // set loading to default / multiplier
+        db_globals.data.config.BaseLoadTime = parseFloat((0.85/config.fastloading.speed_multiplier).toFixed(2));
+    }else{
+        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed.");
+        // set loading to default
+        db_globals.data.config.BaseLoadTime = 0.85;
+    }
+    logger.logSuccess("[MOD] Magazine Maniac loaded.");
+    fileIO.write(db.base.globals, db_globals);
+}
