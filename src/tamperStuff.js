54fd3aae053349a4f5b304f0f3a223c63b84d65d
Benjamin <benjamin.vandeginste@gramontinnovation.com>
2022-01-03 19:46:16 +0100

    made it compatible with AE

--- src/tamperStuff.js
+++ src/tamperStuff.js
@@ -6,12 +6,12 @@ exports.mod = () => {
     if (config.fastloading.enable){
         logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier.");
         // set loading to default / multiplier
-        db_globals.config.BaseLoadTime = parseFloat((0.85/config.fastloading.speed_multiplier).toFixed(2));
+        db_globals.data.config.BaseLoadTime = parseFloat((0.85/config.fastloading.speed_multiplier).toFixed(2));
     }else{
         logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed.");
         // set loading to default
-        db_globals.config.BaseLoadTime = 0.85;
+        db_globals.data.config.BaseLoadTime = 0.85;
     }
-    logger.logSuccess("[MOD] Magazine Maniac loaded.")
+    logger.logSuccess("[MOD] Magazine Maniac loaded.");
     fileIO.write(db.base.globals, db_globals);
 }
