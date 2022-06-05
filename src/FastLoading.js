682e99073f69383a08c55c81bc97375eeb0969fd
Nehax <74174676+Nehaxfr@users.noreply.github.com>
2022-06-05 10:14:56 +0200

    Update FastLoading.js
    removed useless code

--- src/FastLoading.js
+++ src/FastLoading.js
@@ -1,14 +1,9 @@
 exports.mod = () => {
     logger.logSuccess("[MOD] Magazine Maniac loading...");
     let config = require("../config.json");
-    let db_globals = fileIO.readParsed(db.base.globals);
     // set loading to (default/multiplier) if enabled, use default value if not
     if (config.FastLoading.enable){
         logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier.");  
-        db_globals.data.config.BaseLoadTime = parseFloat((0.85/config.FastLoading.speed_multiplier).toFixed(2));
-    }else{
-        logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed.");
-        db_globals.data.config.BaseLoadTime = 0.85;
+        _database.globals.config.BaseLoadTime = parseFloat((0.85/config.FastLoading.speed_multiplier).toFixed(2));
     }
-    _database.globals.config.BaseLoadTime = db_globals.data.config.BaseLoadTime;
 }
